51c46db21df48a4c5781d5cf8e6ac490bbb8770e
ghost-lee-ocean <ghostleeocean@gmail.com>
2021-03-02 16:07:25 -0500

    add middleware get all artists

--- api/api.js
+++ api/api.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const apiRouter = express.Router();
 module.exports = apiRouter;
+
+//import artists router
+const artistsRouter = require('./artists');
+apiRouter.use('/artists', artistsRouter);
---
+++ api/artists.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const artistsRouter = express.Router();
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+artistsRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Artist WHERE Artist.is_currently_employed = 1', 
+    (err, rows) => {
+      if (err) {
+        next(err);
+      } else {
+        res.status(200).json({artists:rows});
+      };
+  });
+});
+
+module.exports = artistsRouter;
