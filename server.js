6f77685176e0befd8e479106469085505613cb51
ghost-lee-ocean <ghostleeocean@gmail.com>
2021-03-02 14:37:56 -0500

    initial express setup

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static('.'));
+
+//body parsing
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//error handling (dev environment only)
+const errorhandler = require('errorhandler');
+app.use(errorhandler());
+
+//cors
+const cors = require('cors');
+app.use(cors());
+
+//morgan
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+  console.log(`Now listening on PORT ${PORT}`);
+});
+
+module.exports = app;
