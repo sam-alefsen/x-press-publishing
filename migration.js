3e02a2fbfcd6e373636828580698f65e92c28f44
ghost-lee-ocean <ghostleeocean@gmail.com>
2021-03-02 15:15:39 -0500

    reinstall sqlite3 and create artist table

---
+++ migration.js
@@ -0,0 +1,7 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./database.sqlite');
+
+db.serialize(() => {
+  db.run('DROP TABLE IF EXISTS Artist');
+  db.run('CREATE TABLE Artist (id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, date_of_birth TEXT NOT NULL, biography TEXT NOT NULL, is_currently_employed INTEGER DEFAULT 1)');
+});
