236a33969f1959d5d6269effd0d855fc0ae83563
ghost-lee-ocean <ghostleeocean@gmail.com>
2021-03-05 16:37:02 -0500

    add artist id check

--- api/artists.js
+++ api/artists.js
@@ -4,6 +4,20 @@ const artistsRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+//Check Artist ID Parameter
+artistsRouter.param('artistId', (req, res, next, artistId) => {
+  db.get('SELECT * FROM Artist WHERE id = $artistId', {$artistId:artistId}, (err, row) => {
+    if (err) {
+      next(err);
+    } else {
+      req.artist = row;
+      next();
+    }
+  });
+});
+
+
+//GET all employed artists
 artistsRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Artist WHERE Artist.is_currently_employed = 1', 
     (err, rows) => {
