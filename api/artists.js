894c85ef19037f8881fb727e42d8f56216f0574e
ghost-lee-ocean <ghostleeocean@gmail.com>
2021-03-08 10:52:05 -0500

    add post artist request

--- api/artists.js
+++ api/artists.js
@@ -36,4 +36,36 @@ artistsRouter.get('/:artistId', (req, res, next) => {
   res.status(200).json({artist:req.artist});
 });
 
+//POST a new artist
+artistsRouter.post('/', (req, res, next) => {
+  const artist = req.body.artist;
+  const isCurrentlyEmployed = artist.isCurrentlyEmployed === 0 ? 0 : 1;
+  if (!artist.name || !artist.dateOfBirth || !artist.biography) {
+    return res.sendStatus(400);
+  } else {
+    //add the new artist
+    db.run('INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed) VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed)', {
+      $name: artist.name,
+      $dateOfBirth: artist.dateOfBirth,
+      $biography: artist.biography,
+      $isCurrentlyEmployed: isCurrentlyEmployed
+    }, function(err) {
+      if(err) {
+        next(err);
+      } else {
+        //Retrieve newly created artist with last ID to place in response. Requires this containing function to not be an arrow function.
+        db.get('SELECT * FROM Artist WHERE ID = $lastID', {
+          $lastID: this.lastID
+        }, (err, row) => {
+          if (err) {
+            next(err);
+          } else {
+            res.status(201).json({artist:row});
+          };
+        });
+      };
+    });
+  };
+});
+
 module.exports = artistsRouter;
